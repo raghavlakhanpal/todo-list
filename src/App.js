e9020a3c4f4152452150aba2ff5da451839e3a53
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2019-04-06 03:56:02 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import './App.css';
 
@@ -33,7 +34,7 @@ class App extends Component {
   }) });
   }
 
-  //delete button
+  //delete todos
   delTodo=(id)=>{
     this.setState({todos:[...this.state.todos.filter(todo => todo.id!==id)]});
   }
@@ -42,6 +43,7 @@ class App extends Component {
    
     return (
       <div className="App">
+        <Header />
         <Todos 
         todos={this.state.todos} 
         markComplete={this.markComplete} 
---
+++ src/components/AddTodo.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export class AddTodo extends Component {
+  render() {
+    return (
+      <form style={{display:flex}}>
+        <input 
+          type="text" 
+          name="title" 
+          placeholder="Add Todo...."
+        />
+        <input 
+          type="submit" 
+          value="submit"
+          className="btn"
+          style={{flex:1}}
+        />
+      </form>
+    );
+  }
+}
+
+export default AddTodo;
