6938cc3d3384b17af53a98711e0ae498da1cf772
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2019-04-06 02:57:55 +0530

    created a delete button

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
       }
     ]
   }
-  
+
   //Toggle Complete
   markComplete=(id)=>{
   this.setState({ todos : this.state.todos.map(todo=>{
@@ -33,13 +33,20 @@ class App extends Component {
   }) });
   }
 
-
+  //delete button
+  delTodo=(id)=>{
+    this.setState({todos:[...this.state.todos.filter(todo => todo.id!==id)]});
+  }
   render() {
 
    
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <Todos 
+        todos={this.state.todos} 
+        markComplete={this.markComplete} 
+        delTodo={this.delTodo}
+        />
       </div>
     );
   }
