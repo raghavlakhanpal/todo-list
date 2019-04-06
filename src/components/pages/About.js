c1bcc69075acbc49aa75771113f74ebe52a11ebb
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2019-04-06 16:54:09 +0530

    created links

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function About() {
+  return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>This is a to-do list app v1.0.0. this is a part of react course.</p>
+    </React.Fragment>
+  )
+}
+export default About;
